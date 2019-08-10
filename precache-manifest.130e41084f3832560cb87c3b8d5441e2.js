05f7b0a5eb00eabfee5a937b0094e734f74315fb
ashertoufeeq <ashertoufeeq@gmail.com>
2019-08-10 20:22:16 +0530

    Updates

---
+++ precache-manifest.130e41084f3832560cb87c3b8d5441e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ce8371dfcd84c43387f8f581d4c93f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3850fe06d14c2f9faa0",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "10842c8a520a5de5839b",
+    "url": "/static/js/2.638c25c2.chunk.js"
+  },
+  {
+    "revision": "a3850fe06d14c2f9faa0",
+    "url": "/static/js/main.066fe87e.chunk.js"
+  },
+  {
+    "revision": "7f74dc577275fad02454",
+    "url": "/static/js/runtime~main.c0fbf7f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
